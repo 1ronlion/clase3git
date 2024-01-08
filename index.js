94a1816b1a035f5205016aca1b63fb85a8bf6da5
1ronlion <leonardo.bana89@gmail.com>
2024-01-08 20:21:40 -0300

    materiales

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("bugs again")
+import obtenerProvinciasSegunMeridiano from './model.js';
+
+function chequearParametros(parametros) {
+    const tieneDosDigitos = parametros[0].length == 2;
+    
+    if(tieneDosDigitos) {
+        const parametroNumerico = Number(parametros[0]);
+        return parametroNumerico;
+    }
+
+    return tieneDosDigitos;
+}
+
+function main() {
+    const parametrosIngresados = process.argv.slice(2);
+    const parametroChequeado = chequearParametros(parametrosIngresados);
+
+    if(!parametroChequeado) {
+        console.log("Parámetros inválidos");
+    } else {
+        const resultado = obtenerProvinciasSegunMeridiano(parametroChequeado);
+        console.log(resultado);
+    }
+}
+
+main();
